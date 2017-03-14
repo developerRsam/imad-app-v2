b8c1b491faad1be20fba9053ce80fbb9f6182307
developerRsam <rahulsamantara27@gmail.com>
2017-03-15 00:13:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,17 @@
+/*console.log('Loaded!');
 
+//Move the image
+var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft = 5;
+    img.style.marginLeft = marginLeft = 'px';
+}
+img.onclick = function(){
+    var interval= setInterval(moveRight, 50);
+};*/
 // counter
-var button= document.getElementById('counter');
+var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function(){
     //Make request to the counter endpoint
@@ -11,4 +22,4 @@ button.onclick = function(){
     counter = counter = 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
-}
+};
